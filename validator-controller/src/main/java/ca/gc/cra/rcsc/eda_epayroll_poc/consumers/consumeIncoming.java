2bb9b9d1af17fdbc537e837c76e65cb8d5c8c848
xln034 <Ashish.Kumar@cra-arc.gc.ca>
2022-03-30 15:45:13 -0400

    Add imports

--- validator-controller/src/main/java/ca/gc/cra/rcsc/eda_epayroll_poc/consumers/consumeIncoming.java
+++ validator-controller/src/main/java/ca/gc/cra/rcsc/eda_epayroll_poc/consumers/consumeIncoming.java
@@ -1,15 +1,10 @@
 package ca.gc.cra.rcsc.eda_epayroll_poc.consumers;
+
 import java.util.*;
 import org.json.*;  
-import ca.gc.cra.rcsc.eda_epayroll_poc.models.*;
 import java.util.Set;
 import java.io.IOException;  
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import ca.gc.cra.rcsc.eda_epayroll_poc.services.ValidateBnService;
-import ca.gc.cra.rcsc.eda_epayroll_poc.services.ValidateEpayrollService;
-import ca.gc.cra.rcsc.eda_epayroll_poc.services.ValidateSinService;
-
+import com.fasterxml.jackson.databind.ObjectMapper;  
 import javax.validation.Valid;
 import javax.validation.Validator;
 import javax.inject.Inject;
@@ -17,14 +12,16 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 import javax.enterprise.context.ApplicationScoped;
-import ca.gc.cra.rcsc.eda_epayroll_poc.services.*;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 
 @ApplicationScoped
