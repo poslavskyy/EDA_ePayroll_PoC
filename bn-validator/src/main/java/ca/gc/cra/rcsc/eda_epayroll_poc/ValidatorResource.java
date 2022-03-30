830711e9acbe3934d4210b71f967aab62c2457f0
xln034 <Ashish.Kumar@cra-arc.gc.ca>
2022-03-30 15:16:54 -0400

    Add neccessary imports

--- bn-validator/src/main/java/ca/gc/cra/rcsc/eda_epayroll_poc/ValidatorResource.java
+++ bn-validator/src/main/java/ca/gc/cra/rcsc/eda_epayroll_poc/ValidatorResource.java
@@ -17,7 +17,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import ca.gc.cra.rcsc.eda_epayroll_poc.ValidatorService;
-
+import javax.ws.rs.core.Response; 
 @Path("/bnValidation")
 public class ValidatorResource {
 
--- sin-validator/src/main/java/ca/gc/cra/rcsc/eda_epayroll_poc/SinResource.java
+++ sin-validator/src/main/java/ca/gc/cra/rcsc/eda_epayroll_poc/SinResource.java
@@ -17,7 +17,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import ca.gc.cra.rcsc.eda_epayroll_poc.*;
-
+import javax.ws.rs.core.Response; 
 
 @Path("/sinValidation")
 public class SinResource {
