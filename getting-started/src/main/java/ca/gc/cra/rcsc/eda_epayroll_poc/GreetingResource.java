86b9ec2b6c7b9cc8495e731178998e1f7c6868e2
Ashish Kumar <Ashish.Kumar@cra-arc.gc.ca>
2022-04-12 20:28:08 +0000

    Add capability to consume messages in getting started

--- getting-started/src/main/java/ca/gc/cra/rcsc/eda_epayroll_poc/GreetingResource.java
+++ getting-started/src/main/java/ca/gc/cra/rcsc/eda_epayroll_poc/GreetingResource.java
@@ -11,7 +11,7 @@ import ca.gc.cra.rcsc.eda_epayroll_poc.GreetingService;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 @Path("/hello")
 public class GreetingResource {
 
@@ -48,4 +48,9 @@ public class GreetingResource {
     	
     	return result;
     }
+
+    @Incoming("getting-started")   
+    public void test_consumer(String obj){
+    	System.out.println("Received msg is "+ obj);
+    } 
 }
